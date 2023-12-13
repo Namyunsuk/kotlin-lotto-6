826a94f7d75a03382c5a699669e4c8d477a1864e
Namyunsuk <yunsuk0328@naver.com>
2023-12-13 22:30:18 +0900

    feat(InputView): 사용자 입력 기능 구현

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,4 +1,26 @@
 package lotto.view
 
+import camp.nextstep.edu.missionutils.Console
+
 class InputView {
+    companion object {
+        fun enterPurchaseAmount(): Int {
+            println("구입금액을 입력해 주세요.")
+            val amount = Console.readLine()
+            return amount.toInt()
+        }
+
+        fun enterPrizeNumber(): List<Int> {
+            println("당첨 번호를 입력해 주세요.")
+            val userInput = Console.readLine()
+            val prizeNumbers = userInput.split(",")
+            return prizeNumbers.map { it.toInt() }
+        }
+
+        fun enterBonusNumber(): Int {
+            println("보너스 번호를 입력해 주세요.")
+            val bonusNumber = Console.readLine()
+            return bonusNumber.toInt()
+        }
+    }
 }
