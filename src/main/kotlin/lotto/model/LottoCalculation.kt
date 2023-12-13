fd3beb589584a775b53d8eeded607e51683632c5
Namyunsuk <yunsuk0328@naver.com>
2023-12-13 23:26:08 +0900

    feat(LottoCalculation): 구입금액에 따른 로또 수 계산

--- src/main/kotlin/lotto/model/LottoCalculation.kt
+++ src/main/kotlin/lotto/model/LottoCalculation.kt
@@ -4,4 +4,12 @@ class LottoCalculation {
     fun calculateProfitRate(purchaseAmount: Int, totalPrize: Int): Float {
         return (totalPrize.toFloat() / purchaseAmount) * 100
     }
+
+    fun calculateLottoAmount(money: Int): Int {
+        return money/LOTTO_PRICE
+    }
+
+    companion object{
+        const val LOTTO_PRICE = 1000
+    }
 }
